6c53c4ee0c8e91f60a87d6eb5b5031f128b6e855
Vaishnavi Vaidya <vaishnavi.vaidya@flairminds.com>
2024-10-18 11:51:03 +0530

    combined const values in one object for export in urlConstant.jsx

--- src/services/apiServices.jsx
+++ src/services/apiServices.jsx
@@ -1,26 +1,26 @@
-import { GET_CHART, GET_SCRIPT, GET_TABLE, LOGIN, SIGNUP, UPLOAD_DOCUMENT } from "../utils/urlConstant";
+import { API_URL } from "../utils/urlConstant";
 import { HttpGet, HttpPost } from "./base";
 
 export const loginUser = async (email, password) => {
-	return await HttpPost(LOGIN, {email, password});
+	return await HttpPost(API_URL.AUTH_SERVICE.LOGIN, {email, password});
 };
 
 export const signinUser = async (payload) => {
-	return await HttpPost(SIGNUP, payload);
+	return await HttpPost(API_URL.AUTH_SERVICE.SIGN_UP, payload);
 };
 
 export const uploadDocument = async (id, payload) => {
-	return await HttpPost(`${UPLOAD_DOCUMENT}/${id}`, payload );
+	return await HttpPost(`${API_URL.DOCUMENT.UPLOAD_DOCUMENT}/${id}`, payload );
 };
 
 export const getTable = async (signal) => {
-	return await HttpGet(`${GET_TABLE}`, {}, {}, signal);
+	return await HttpGet(API_URL.DOCUMENT.GET_TABLE, {}, {}, signal);
 };
 
 export const getChart = async (userId, chartName, signal) => {
-	return await HttpGet(`${GET_CHART}/${7}?graph_name=${chartName}`, {}, {}, signal);
+	return await HttpGet(`${API_URL.CHART.GET_CHART}/${userId}?graph_name=${chartName}`, {}, {}, signal);
 };
 
 export const getCodeScript = async (chartName, signal) => {
-	return await HttpGet(`${GET_SCRIPT}?graph_name=${chartName}`, {}, {}, signal);
+	return await HttpGet(`${API_URL.CHART.GET_SCRIPT}?graph_name=${chartName}`, {}, {}, signal);
 };
