886470f73e0bf514668b23786ffd4b6867bd105d
punit-fm <punit.suman@flairminds.com>
2024-11-27 16:50:51 +0530

    drawdown chart issue resolved

--- src/components/elements/Table/Table.jsx
+++ src/components/elements/Table/Table.jsx
@@ -2,26 +2,29 @@ import React from 'react';
 import styles from './Table.module.css';
 
 export default function Table({ tableData }) {
-	const headers = Object.keys(tableData[0]);
-
-	return (
-		<table className={styles.table}>
-			<thead>
-				<tr className={styles.tableRow}>
-					{headers.map((header, index) => (
-						<th className={styles.tableHead} key={index}>{header.replace("_", " ")}</th>
-					))}
-				</tr>
-			</thead>
-			<tbody>
-				{tableData.map((row, rowIndex) => (
-					<tr className={styles.tableRow} key={rowIndex}>
-						{headers.map((header, cellIndex) => (
-							<td className={styles.tableData} key={cellIndex}>{row[header]}</td>
+	if (!tableData || !tableData[0]) {
+		return <div>No data</div>
+	} else {
+		const headers = Object.keys(tableData[0]);
+		return (
+			<table className={styles.table}>
+				<thead>
+					<tr className={styles.tableRow}>
+						{headers.map((header, index) => (
+							<th className={styles.tableHead} key={index}>{header.replace("_", " ")}</th>
 						))}
 					</tr>
-				))}
-			</tbody>
-		</table>
-	);
+				</thead>
+				<tbody>
+					{tableData.map((row, rowIndex) => (
+						<tr className={styles.tableRow} key={rowIndex}>
+							{headers.map((header, cellIndex) => (
+								<td className={styles.tableData} key={cellIndex}>{row[header]}</td>
+							))}
+						</tr>
+					))}
+				</tbody>
+			</table>
+		);
+	}
 }
